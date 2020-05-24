d911a70bbb4bfb767f196649ab24563797a26b15
Rishabh Nautiyal <naytiyalrishabh99@gmail.com>
2020-05-24 18:39:20 +0800

    First

---
+++ js/1516A.js
@@ -0,0 +1,22 @@
+function initMenu() {
+  return [
+    {"description":"Picture 1",
+      "imgLink":"img/collection/15-16SemA/1.jpg",
+    },
+    {"description":"Picture 2",
+      "imgLink":"img/collection/15-16SemA/2.jpg",
+    },
+    {"description":"Picture 3",
+      "imgLink":"img/collection/15-16SemA/3.jpg",
+    },
+    {"description":"Picture 4",
+      "imgLink":"img/collection/15-16SemA/4.jpg",
+    },
+    {"description":"Picture 5",
+      "imgLink":"img/collection/15-16SemA/5.jpg",
+    },
+    {"description":"Picture 6",
+      "imgLink":"img/collection/15-16SemA/6.jpg",
+    }
+  ]
+  }
---
+++ js/1516B.js
@@ -0,0 +1,22 @@
+function initMenu() {
+  return [
+    {"description":"Picture 1",
+      "imgLink":"img/collection/15-16SemB/1.jpg",
+    },
+    {"description":"Picture 2",
+      "imgLink":"img/collection/15-16SemB/2.jpg",
+    },
+    {"description":"Picture 3",
+      "imgLink":"img/collection/15-16SemB/3.jpg",
+    },
+    {"description":"Picture 4",
+      "imgLink":"img/collection/15-16SemB/4.jpg",
+    },
+    {"description":"Picture 5",
+      "imgLink":"img/collection/15-16SemB/5.jpg",
+    },
+    {"description":"Picture 6",
+      "imgLink":"img/collection/15-16SemB/6.jpg",
+    }
+  ]
+  }
